e9f685ce83045c59f9aa17c37ddd4c720af6dde6
PyDev19 <atharvamishra956+1@gmail.com>
2023-05-17 18:14:05 -0500

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,73 @@
+#include <Arduino.h>
+#include <DHT.h>
+#include <Adafruit_BMP085.h>
+#include <SPI.h>
+
+#define DHTTYPE DHT11
+#define DHTPIN 5
+
+struct WeatherTuple {
+	int temperature;
+	int temperature_f;
+	int humidity;
+	int pressure;
+};
+
+DHT dht(DHTPIN, DHTTYPE);
+Adafruit_BMP085 bmp;
+
+int unit = 0;
+
+/**
+ * Gets the pressure, humidity, and temperature from BMP180 and DHT11 sensor.
+ * @param dht DHT11 sensor object
+ * @param bmp BMP180 sensor object
+ * @param unit Unit of the temperature, O for SI units, 1 for CGS units
+ * @return A WeatherTuple that contains temperature, humidity, and pressure
+*/
+WeatherTuple get_weather(DHT dht, Adafruit_BMP085 bmp, int unit);
+
+void setup() {
+	Serial.begin(9600);
+
+	dht.begin();
+	if (!bmp.begin()) {
+		Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+		while (1) {}
+  	}
+}
+
+void loop() {
+	delay(500);
+
+	WeatherTuple weather;
+	weather = get_weather(dht, bmp, unit);
+}
+
+WeatherTuple get_weather(DHT dht, Adafruit_BMP085 bmp, int unit) {
+	WeatherTuple weather;
+	int humidity, temperature_f, temperature_c, pressure, heat_index;
+	
+	temperature_c = bmp.readTemperature();
+	temperature_f = (temperature_c * (9.0/5.0)) + 32.0;
+	humidity = dht.readHumidity();
+	pressure = bmp.readPressure()*0.01;
+
+	if (isnan(humidity) || isnan(temperature_c)) {
+		Serial.println(F("Failed to read from DHT sensor!"));
+
+		weather.temperature = 0;
+		weather.temperature_f = 0;
+		weather.humidity = 0;
+		weather.pressure = 0;
+
+		return weather;
+	}
+
+	weather.temperature = temperature_c;
+	weather.temperature_f = temperature_f;
+	weather.humidity = humidity;
+	weather.pressure = pressure;
+
+	return weather;
+}
