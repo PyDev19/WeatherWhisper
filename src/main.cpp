1ea0ccd36c938c6db511388bdbaf793a96ee928a
PyDev19 <atharvamishra956+1@gmail.com>
2023-05-20 12:20:58 -0500

    Added LCD to display weather condition
    Added LCD to display the weather conditions and if button is pressed you can change the units from celsius to fahrenheit and vice versa

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,12 @@
 #include <DHT.h>
 #include <Adafruit_BMP085.h>
 #include <SPI.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <ezButton.h>
 
+#define BMP_I2C 0x77
+#define LCD_12C 0x27
 #define DHTTYPE DHT11
 #define DHTPIN 5
 
@@ -13,8 +18,21 @@ struct WeatherTuple {
 	int pressure;
 };
 
+byte degree_symbol[8] = {
+  B01110,
+  B01010,
+  B01110,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+};
+
 DHT dht(DHTPIN, DHTTYPE);
 Adafruit_BMP085 bmp;
+LiquidCrystal_I2C lcd(LCD_12C, 20, 2);
+ezButton unitchange_button(12);
 
 int unit = 0;
 
@@ -28,20 +46,58 @@ int unit = 0;
 WeatherTuple get_weather(DHT dht, Adafruit_BMP085 bmp, int unit);
 
 void setup() {
+	Wire.begin();
 	Serial.begin(9600);
 
+	unitchange_button.setDebounceTime(50);
+
 	dht.begin();
+	lcd.begin(20, 2);
 	if (!bmp.begin()) {
 		Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 		while (1) {}
   	}
+
+	lcd.backlight();
+	lcd.createChar(1, degree_symbol);
 }
 
 void loop() {
-	delay(500);
+	unitchange_button.loop();
+	if (unitchange_button.isPressed()) {
+		switch (unit) {
+			case 0:
+				unit = 1;
+				break;
+			case 1:
+				unit = 0;
+				break;
+		}
+	}
 
 	WeatherTuple weather;
 	weather = get_weather(dht, bmp, unit);
+
+	// Display the data on the LCD display
+	lcd.setCursor(0, 0);
+	lcd.print("Temp: ");
+	switch (unit) {
+		case 0:
+			lcd.print(weather.temperature);
+			lcd.write(1);
+			lcd.print("C");
+			break;
+		case 1:
+			lcd.print(weather.temperature_f);
+			lcd.write(1);
+			lcd.print("F");
+			break;
+	}
+	
+	lcd.setCursor(0, 1);
+	lcd.print("Humidity: ");
+	lcd.print(weather.humidity);
+	lcd.print("%");
 }
 
 WeatherTuple get_weather(DHT dht, Adafruit_BMP085 bmp, int unit) {
